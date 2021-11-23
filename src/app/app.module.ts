cbb7ae4fbbb5a0b05526fbd88bba4f5c6f98225d
datgerhardt <datsomorg@gmail.com>
2021-11-23 14:14:32 +0000

    add drag and drop functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DragDropModule } from '@angular/cdk/drag-drop'
 import { TaskComponent } from './task/task.component'
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +20,10 @@ import { TaskComponent } from './task/task.component'
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
